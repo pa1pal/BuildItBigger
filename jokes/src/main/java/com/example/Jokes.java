25b9957f02ead8df67ae005e87ae5d21326e711b
pa1pal <pawanpal004@gmail.com>
2016-09-16 00:44:40 +0530

    added Jokes provider module

--- jokes/src/main/java/com/example/Jokes.java
+++ jokes/src/main/java/com/example/Jokes.java
@@ -1,4 +1,19 @@
 package com.example;
 
+import java.util.ArrayList;
+
 public class Jokes {
+
+    private ArrayList<String> jokes = new ArrayList<String>();
+    public Jokes() {
+        jokes.add("example");
+        jokes.add("example");
+        jokes.add("example");
+        jokes.add("example");
+        jokes.add("example");
+        jokes.add("example");
+        jokes.add("example");
+        jokes.add("example");
+        jokes.add("example");
+    }
 }
