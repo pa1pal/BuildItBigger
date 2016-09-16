e0ccd7dd3ab3b9c432788bcc8ed167a4ff92b131
pa1pal <pawanpal004@gmail.com>
2016-09-16 17:52:50 +0530

    added Android Library

---
+++ jokesview/src/main/java/pa1pal/udacity/jokesview/JokesView.java
@@ -0,0 +1,17 @@
+package pa1pal.udacity.jokesview;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * User: pa1pal
+ * Date: 9/16/16
+ */
+public class JokesView extends ActionBarActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.jokes_view);
+    }
+}
